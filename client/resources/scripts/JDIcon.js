80c9dbf7b75fdd7acb11f9d489e265cae635d6f1
Jérémie Dupuis <JeremieD@users.noreply.github.com>
2022-04-27 00:03:30 -0400

    Add cache and size placeholder

--- client/resources/scripts/JDIcon.js
+++ client/resources/scripts/JDIcon.js
@@ -6,12 +6,33 @@ class JDIcon extends HTMLElement {
 	constructor() {
 		super();
 
+	}
+
+	connectedCallback() {
+		if (!this.hasAttribute("icon")) {
+			return;
+		}
+
 		const iconName = encodeURI(this.getAttribute("icon"));
 
-		httpGet("/resources/graphics/icons/" + iconName + ".svg").then(svg => {
+		if (JDIcon.cache[iconName] !== undefined) {
+			JDIcon.cache[iconName].then(svg => {
+				this.innerHTML = svg;
+			});
+			return;
+		}
+
+		this.classList.add("placeholder");
+
+		let iconPath = "/resources/graphics/icons/" + iconName + ".svg";
+		JDIcon.cache[iconName] = httpGet(iconPath).then(svg => {
 			this.innerHTML = svg;
+			this.classList.remove("placeholder");
+			return svg;
 		});
 	}
+
+	static cache = {};
 }
 
 customElements.define("jd-icon", JDIcon);
